6c6d5438057ad3817b9c58b18f9b73fca2d80a1b
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-21 16:48:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,4 +4,16 @@ class Pokemon
   def initialize(info_hash)
     info_hash.each{|key, value| self.send(("#{key}="), value)}
   end
+
+  def save
+  if self.id
+    self.update
+  else
+    sql = <<-SQL
+      INSERT INTO pokemon (id, name, type)
+      VALUES (?, ?, ?)
+    SQL
+    DB[:conn].execute(sql, self.id, self.name, self.type)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
 end
